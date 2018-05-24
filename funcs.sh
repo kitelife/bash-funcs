4ea0747e65b816eb2787f7633b82ae91592f4c87
xiayongfeng <xiayongfeng@baidu.com>
2018-05-24 10:54:54 +0800

    add funcs

--- funcs.sh
+++ funcs.sh
@@ -96,3 +96,15 @@ sum() {
     done
     echo $v
 }
+
+str_lower() {
+    echo "$@" | tr '[:upper:]' '[:lower:]'
+    # or
+    # echo "$@" | awk '{print tolower($0)}'
+}
+
+str_upper() {
+    echo "$@" | tr '[:lower:]' '[:upper:]'
+    # or
+    # echo "$@" | awk '{print toupper($0)}'
+}
